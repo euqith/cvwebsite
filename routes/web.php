4a75a625a0eb102aaa056a283a3d5d177aafd1fd
Christian <christian.eyd@gmail.com>
2025-03-02 10:50:50 +0700

    commit-initializing-login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\headerController;
+use App\Http\Controllers\loginController;
 
 
 Route::get('/',  [headerController::class, 'index']);
+Route::get('/login', [loginController::class, 'index']);
 
 
